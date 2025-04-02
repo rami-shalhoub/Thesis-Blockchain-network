b00ceb385f15768cd526a08d999cb9282fc20eac
rami shalhoub <rami2002.work@gmail.com>
2025-04-02 22:20:48 +0200

    minor fixes, adding IBM copyright

--- ccp/ccp-generate.sh
+++ ccp/ccp-generate.sh
@@ -32,28 +32,28 @@ ORG=client
 anchorPORT=7000
 backupPORT=7010
 CAPORT=7020
-PEERPEM=orgCrypto/peerOrganizations/clientOrg.example.com/tlsca/tlsca.clientOrg.example.com-cert.pem
-CAPEM=orgCrypto/peerOrganizations/clientOrg.example.com/ca/ca.clientOrg.example.com-cert.pem
+PEERPEM=orgCrypto/peerOrganizations/client.example.com/tlsca/tlsca.client.example.com-cert.pem
+CAPEM=orgCrypto/peerOrganizations/client.example.com/ca/ca.client.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/clientOrg.example.com/connection-clientOrg.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto  /peerOrganizations/clientOrg.example.com/connection-clientOrg.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/client.example.com/connection-clientOrg.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto  /peerOrganizations/client.example.com/connection-clientOrg.yaml
 
 ORG=lawfirm
 anchorPORT=8000
 backupPORT=8010
 CAPORT=8020
-PEERPEM=orgCrypto/peerOrganizations/lawfirmOrg.example.com/tlsca/tlsca.lawfirmOrg.example.com-cert.pem
-CAPEM=orgCrypto/peerOrganizations/lawfirmOrg.example.com/ca/ca.lawfirmOrg.example.com-cert.pem
+PEERPEM=orgCrypto/peerOrganizations/lawfirm.example.com/tlsca/tlsca.lawfirm.example.com-cert.pem
+CAPEM=orgCrypto/peerOrganizations/lawfirm.example.com/ca/ca.lawfirm.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/lawfirmOrg.example.com/connection-lawfirmOrg.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/lawfirmOrg.example.com/connection-lawfirmOrg.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/lawfirm.example.com/connection-lawfirmOrg.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/lawfirm.example.com/connection-lawfirmOrg.yaml
 
 ORG=retail
 anchorPORT=9000
 backupPORT=9010
 CAPORT=9020
-PEERPEM=orgCrypto/peerOrganizations/retailOrg.example.com/tlsca/tlsca.retailOrg.example.com-cert.pem
-CAPEM=orgCrypto/peerOrganizations/retailOrg.example.com/ca/ca.retailOrg.example.com-cert.pem
+PEERPEM=orgCrypto/peerOrganizations/retial.example.com/tlsca/tlsca.retial.example.com-cert.pem
+CAPEM=orgCrypto/peerOrganizations/retial.example.com/ca/ca.retial.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/retailOrg.example.com/connection-retailOrg.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/retailOrg.example.com/connection-retailOrg.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/retial.example.com/connection-retailOrg.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > orgCrypto/peerOrganizations/retial.example.com/connection-retailOrg.yaml
